20b63fa0652ad8fede31dd08d52ad107648f98b6
huayemao <huayemao4g@gmail.com>
2022-12-31 21:34:15 +0800

    fix: 尝试编程式指明 db 路径

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import path from "path";
 
-export * from '@prisma/client'
+export * from "@prisma/client";
 
-let prisma: PrismaClient
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient({
-    errorFormat: 'minimal',
-  })
+    datasources: {
+      db: {
+        url: "file:" + path.join(process.cwd(), "tmp/dev.db"),
+      },
+    },
+    errorFormat: "minimal",
+  });
 } else {
-  globalThis['prisma'] =
-    globalThis['prisma'] ||
+  globalThis["prisma"] =
+    globalThis["prisma"] ||
     new PrismaClient({
-      errorFormat: 'pretty',
-    })
-  prisma = globalThis['prisma']
+      errorFormat: "pretty",
+    });
+  prisma = globalThis["prisma"];
 }
 
-export default prisma
+export default prisma;
