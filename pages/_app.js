a780c9526a1829641a84a163c774813538d021cb
huayemao <huayemao4g@gmail.com>
2025-12-01 11:04:44 +0800

    fix: 修复构建错误

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/index.css'
-
+import { Analytics } from "@vercel/analytics/react"
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Analytics r/>
+          {/* <Analytics /> */}
         </body>
       </Html>
     )
